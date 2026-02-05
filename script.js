d4635b28b21789b198c013f6d353470e63ddd80a
sabareesh2112 <siliverusabareesh@gmail.com>
2026-02-05 17:30:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change navbar color on scroll
+window.addEventListener("scroll", function () {
+  let navbar = document.getElementById("navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
